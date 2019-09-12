2f57087922cfce43e396fc6f2d47187f4490f3e8
Souporno Ghosh <souporno2000@gmail.com>
2019-09-12 16:57:55 +0530

    Update app.js
    Reorganised imports

--- server/app.js
+++ server/app.js
@@ -1,18 +1,15 @@
 const express = require('express');
-
 const app = express();
-
 const mongoose = require('mongoose');
-
 const bodyParser = require('body-parser');
-
-const uri = 'mongodb://localhost:27017/TestProject';
-
 const cors = require('cors');
 
 const Routes = require('./routes');
 
+const uri = 'mongodb://localhost:27017/TestProject';
+
 mongoose.set('useFindAndModify', false);
+
 app.use(cors());
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
